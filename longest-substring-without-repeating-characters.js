04622e51fc9e918d9273f829d5847ec702e7d3ce
Cardoso88 <alexmcardoso@gmail.com>
2025-06-25 15:47:40 -0300

    add longest-substring-without-repeating-characters

---
+++ longest-substring-without-repeating-characters.js
@@ -0,0 +1,79 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  if (s.length <= 1) return s.length
+  let letter = new Set()
+  let count = 0
+  let biggest = 0
+  for (let i = 0; i < s.length; i++) {
+    letter.clear()
+    count = 0
+    if (biggest + i >= s.length) break
+    for (let j = i; j < s.length; j++) {
+      if (!letter.has(s[j])){
+        letter.add(s[j])
+        count++
+      } else {
+        break
+      }
+    }
+    if(biggest < count) biggest = count    
+  }
+  return biggest
+};
+
+
+var lengthOfLongestSubstringBest = function(s) {
+  let current = "";  // store current substring without repeats
+    let maxLen = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        let ch = s[i];
+
+        // If character is already in current string, remove up to that point
+        if (current.includes(ch)) {
+            let index = current.indexOf(ch);
+            current = current.slice(index + 1);
+        }
+
+        current += ch;  // Add current character
+        maxLen = Math.max(maxLen, current.length);  // Update max
+    }
+
+    return maxLen;
+    
+};
+
+// console.log(lengthOfLongestSubstring("abcabcbb"))
+// console.log(lengthOfLongestSubstring("bbbbb"))
+// console.log(lengthOfLongestSubstring("pwwkew"))
+// console.log(lengthOfLongestSubstring(""))
+// console.log(lengthOfLongestSubstring("asdfg"))
+console.log(lengthOfLongestSubstringBest("dvdf"))
+
+
+
+
+
+
+
+ // if (s.length <= 1) return s.length
+ // let letter = new Set()
+ // let count = 0
+ // let biggest = 0
+ // for (let i = 0; i < s.length; i++) {
+ //   if(biggest < count) biggest = count
+ //   letter.clear()
+ //   count = 0
+ //   for (let j = i; j < s.length; j++) {
+ //     if (!letter.has(s[j])){
+ //       count++
+ //     } else {
+ //       exit
+ //     }
+ //     letter.add(s[j])
+ //   }
+ // }
+ // return biggest > count ? biggest : count
